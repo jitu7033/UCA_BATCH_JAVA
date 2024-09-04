53bd134cebfcfbfeaffd0187c0d1a0bb9618581e
jitu7033 <jraj4797@gmail.com>
2024-09-04 14:35:02 +0530

    minHeap Implemented and tested it is working now

--- src/Heap/maxHeapUsingLinkedList1.java
+++ src/Heap/maxHeapUsingLinkedList1.java
@@ -3,17 +3,31 @@ package Heap;
 
 public class maxHeapUsingLinkedList1 {
     public static void main(String[] args) {
+
+        // max heap are int top
+
         maxHeap maxHeap = new maxHeap();
-        maxHeap.insert(9);
-        maxHeap.insert(10);
         maxHeap.insert(11);
-        maxHeap.insert(12);
-        maxHeap.insert(90);
+        maxHeap.insert(1000);
+        maxHeap.insert(-110);
         System.out.println(maxHeap.peek());
         maxHeap.remove();
-        maxHeap.insert(200);
-        maxHeap.remove();
-        System.out.println(maxHeap.peek());
         maxHeap.print();
+        System.out.println();
+
+        // min heap are in bottom
+
+        minHeap minHeap = new minHeap();
+        minHeap.insert(11);
+        minHeap.insert(10);
+        minHeap.insert(0);
+        minHeap.insert(-110000);
+        minHeap.insert(10000);
+        minHeap.remove();
+        minHeap.remove();
+        System.out.println(minHeap.peek());
+        minHeap.print();
+
+        // testing completed it is working now finally good
     }
 }
